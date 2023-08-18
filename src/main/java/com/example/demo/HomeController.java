a0ae3b51cfc22c00f97146a1fdb601cb2bde718b
David Nicola <davidnicola1098@hotmail.com>
2023-08-17 23:02:03 -0500

    ultimo commit

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+
 import java.util.List;
 import java.util.Map;
 
@@ -24,8 +25,8 @@ public class HomeController {
 
 	@GetMapping(path = "/api/bandas/{id}/formacion")
 	public @ResponseBody List<Map <String, Object>> formacion(@PathVariable Integer id){
-		String sql ="";
-		List<Map <String, Object>> queryResult = jdbcTemplate.queryForList(sql);
+		String sql = "SELECT integrante.id as ID, musico.nombre as MUSICO, instrumento.nombre as INSTRUMENTO FROM integrante JOIN musico ON integrante.id_musico=musico.id JOIN instrumento ON integrante.id_instrumento=instrumento.id WHERE integrante.id_banda = ?";
+		List<Map <String, Object>> queryResult = jdbcTemplate.queryForList(sql, id);
 		return queryResult;
 	}
 
