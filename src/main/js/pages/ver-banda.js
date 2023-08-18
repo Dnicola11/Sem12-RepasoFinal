d304d1dc295a95dabd5bdca0639127d5abf335ba
David Nicola <davidnicola1098@hotmail.com>
2023-08-17 22:57:17 -0500

    32322332

--- src/main/js/pages/ver-banda.js
+++ src/main/js/pages/ver-banda.js
@@ -7,14 +7,18 @@ const VerBandaPage = () => {
 
     let { id } = useParams();
     const [banda, setBanda] = useState({});
+    const [integrantes, setIntegrantes] = useState([]);
 
     useEffect(()=>{
     client({
         method: 'GET',
         path: '/api/bandas/' + id
     }).done(response=>setBanda(response.entity))
+    client({
+        method: 'GET',
+        path: '/api/bandas/' + id + '/formacion'
+    }).done(response=>setIntegrantes(response.entity))
     },[])
-
 
     return (
         <>
@@ -27,14 +31,29 @@ const VerBandaPage = () => {
                 </tr>
             </table>
             <hr/>
+            
             <h2>Formacion</h2>
             <table border="1">
+                <thead>
                 <tr>
-                    <th>Nombre</th>
-                    <td>{banda.nombre}</td>
+                    <th>Musico</th>
+                    <td>Instrumento</td>
                 </tr>
+                </thead>
+                <tbody>
+                    {integrantes.map(integrante=>{
+                        return(
+                            <tr key={integrante.ID}>
+                                <td>{integrante.MUSICO}</td>
+                                <td>{integrante.INSTRUMENTO}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
             </table>
 
+            <hr />
+            <Link to={'/ver-banda/${id}/nuevo-integrante'}>Nuevo Integrante</Link> | 
             <Link to="/">Volver</Link>
         </>
     )
